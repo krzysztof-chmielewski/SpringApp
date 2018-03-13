e6286c6586fb7171823a3d5e50d4d94fef3d11b0
Krystian Jagoda <krystian.jagoda@outlook.com>
2018-03-14 00:22:17 +0100

    Configure environment

--- src/main/java/uk/co/krystianjagoda/springapp/Initializer/Initializer.java
+++ src/main/java/uk/co/krystianjagoda/springapp/Initializer/Initializer.java
@@ -1,4 +1,21 @@
 package uk.co.krystianjagoda.springapp.Initializer;
 
-public class Initializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import uk.co.krystianjagoda.springapp.config.Config;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
--- src/main/java/uk/co/krystianjagoda/springapp/config/Config.java
+++ src/main/java/uk/co/krystianjagoda/springapp/config/Config.java
@@ -1,4 +1,26 @@
 package uk.co.krystianjagoda.springapp.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import uk.co.krystianjagoda.springapp.app.PlayerController;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = PlayerController.class)
 public class Config {
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
 }
